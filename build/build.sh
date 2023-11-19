74415b5df9a7ed03a493e53d4ec63a4975bb9e43
Daniel Anker Hermansen <daniel201001@gmail.com>
2023-11-19 15:20:06 +0100

    bump dep

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 wasm-pack build ../frontend --no-typescript --target web --out-dir ../build/pkg
 cd ../backend
-cargo build
+cargo build --release
 cd ../build
-mv ../backend/target/debug/backend backend
+mv ../backend/target/release/backend backend
 ./backend Config.toml
