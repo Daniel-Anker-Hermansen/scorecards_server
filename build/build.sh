37788f9169529e5cc44b65fe3a78fab851f4f59a
Daniel Anker Hermansen <daniel201001@gmail.com>
2025-11-01 21:35:58 +0100

    clean

--- build/build.sh
+++
@@ -1,6 +0,0 @@
-wasm-pack build ../frontend --no-typescript --target web --out-dir ../build/pkg
-cd ../backend
-cargo build --release
-cd ../build
-mv ../backend/target/release/backend backend
-./backend Config.toml
